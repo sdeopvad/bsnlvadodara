d43e2e7867a340189a8f967d8034a3d04b511869
sdeopvad <121152246+sdeopvad@users.noreply.github.com>
2022-12-21 12:00:06 +0530

    Add files via upload

---
+++ mylinks.php
@@ -0,0 +1,43 @@
+<html>
+<body>
+<div id="templatemo_left_column">        	
+
+			
+            	<h2>Links</h2>
+                <ul>
+					<LI> 	<a href="dailyrep1.htm" target="_blank">Daily Report</a></LI>
+					<LI> 	<a href="dailyrep1v7.htm" target="_blank">Daily Report V7 Cluster wise</a></LI>
+					<LI> 	<a href="dailyrepv7.htm" target="_blank">Daily Report V7</a></LI>
+					<LI> 	<a href="dailyfaultrep.htm" target="_blank">Daily fault Report</a></LI>
+					<LI> 	<a href="faultreport.htm" target="_blank">Daily fault Report (New)</a></LI>
+					<LI> 	<a href="llfaults.htm" target="_blank">LL Fault</a></LI>
+					<LI> 	<a href="bbfaults.htm" target="_blank">BB Fault</a></LI>
+					<LI> 	<a href="ll30days.htm" target="_blank">LL 30 days Fault</a></LI>
+					<LI> 	<a href="bb30days.htm" target="_blank">BB 30 days Fault</a></LI>
+					<LI> 	<a href="cluster_perf_rep.htm" target="_blank">Cluster Performance report</a></LI>
+					<LI> 	<a href="dailyfaultsumm.htm" target="_blank">Daily fault summary</a></LI>
+					<LI> 	<a href="ftth_so.htm" target="_blank">FTTH SO</a></LI>
+					<LI> 	<a href="https://tinyurl.com/yal2v3dg" target="_blank">Circle OFC G-Sheet</a></LI>
+					<LI> 	<a href="https://tinyurl.com/y82lsmfx" target="_blank">Local OFC G-Sheet</a></LI>
+					<LI> 	<a href="https://bit.ly/2VFPulg" target="_blank">Local OFC G-Form</a></LI>
+					<LI> 	<a href="https://bit.ly/3aYnQG1" target="_blank">Local Cable G-Form</a></LI>
+					<LI> 	<a href="https://bit.ly/2UMy9r4" target="_blank">Local Cable G-Sheet</a></LI>
+					<LI> 	<a href="https://tinyurl.com/y9chxzkk" target="_blank">PG Cases G-Sheet</a></LI>
+								
+					
+					
+					
+					
+					
+					<!--<LI>    <a href="vad_userlist.php"><b>User List</b></a></LI>  -->
+					<LI>    <a href="off_dir.php"><b>Officer's Directory</b></a></LI>
+                </ul>
+            </div>
+            
+			<!-- <div id="imagebutton">
+            	<a href="#"><img src="images/templatemo_livechat.gif" alt="live chat" /></a>
+			</div> -->
+            
+        </div>
+</body>
+</html>
